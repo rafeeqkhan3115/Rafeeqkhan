7c86ee34e373a0af332427978410d5f34747ab88
rafeeqkhan3115 <67560731+rafeeqkhan3115@users.noreply.github.com>
2021-12-08 04:10:47 -0800

    Add files via upload

---
+++ Bomber.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+clear
+echo -e "Welcome to Bomber"
+echo "Press Enter To Continue"
+read a1
+if [[ -s update.Rafeeqkhan ]];then
+echo "All Requirements Found...." | lolcat
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+apt install boxes -y
+fi
+else
+echo 'Installing Requirements....'
+echo .
+echo .
+apt install ruby figlet fortune cowsay -y
+apt install figlet toilet python curl -y
+apt install python3-pip
+pip install -r requirements.txt
+git clone https://github.com/busyloop/lolcat
+cd lolcat
+cd bin
+gem install lolcat
+cd ..
+cd ..
+rm -rf lolcat
+echo This Script Was Made By rafeeqkhan_official >update.rafeeqkhan_official
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+echo -e "\e[1;34m Created By \e[1;32m" | lolcat
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+clear
+toilet -f ivrit 'Rafeeqkhan' | boxes -d cat -a hc -p h8 | lolcat
+echo -e "\e[4;34m This Bomber Was Created By rafeeqkhan\e[0m" | lolcat
+echo -e "\e[1;34m For Any Queries Contact Me!!!\e[0m"| lolcat
+echo -e "\e[1;32m   Intagram:- rafeeqkhan_official"| lolcat
+echo " "| lolcat
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"| lolcat
+echo " "| lolcat
+echo "Press 1 To  Start SMS Bomber "| lolcat
+echo "Press 2 To  Start Call Bomber "| lolcat
+echo "Press 3 To  Update "| lolcat
+echo "Press 4 To  View Features "| lolcat
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 bomber.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By Rafeeqkhan_official'> call.xxx
+python3 bomber.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..." | lolcat
+git clone https://github.com/pepperjack1210/Bomber
+if [[ -s Bomber/Bomber.sh ]];then
+cd Bomber
+cp -r -f * .. > temp
+cd ..
+rm -rf  Bomber >> temp
+rm update.Rafeeqkhan_official >> temp
+rm temp
+chmod +x Bomber.sh
+fi
+echo -e "\e[1;32m Bomber Will Restart Now..."| lolcat
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."| lolcat
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."| lolcat
+read a6
+./Bomber.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"| lolcat
+figlet Bomb | lolcat
+echo -e "\e[1;34mCreated By \e[1;34m"| lolcat
+toilet -f mono12 -F border rafeeqkhan_official | lolcat
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m" | lolcat
+echo "  [+] Unlimited And Super-Fast Bombing"| lolcat
+echo "  [+] International Bombing"| lolcat
+echo "  [+] Call Bombing "| lolcat
+echo "  [+] Protection List"| lolcat
+echo "  [+] Automated Future Updates"| lolcat
+echo "  [+] Easy To Use And Embed in Code"| lolcat
+echo ""
+echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"| lolcat
+echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"| lolcat
+echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"| lolcat
+echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"| lolcat
+echo  " "
+echo -e "\e[4;31m That's All !!!\e[0m"| lolcat
+echo -e "\e[4;34m This Bomber Was Created By rafeeqkhan_official \e[0m" | lolcat
+echo -e "\e[1;34m For Any Queries Contact Me!!!\e[0m"| lolcat
+echo -e "\e[1;32m   Intagram:- rafeeqkhan_official"| lolcat
+echo "Press Enter To Go Home"| lolcat
+read a3
+clear
+else
+echo -e "\e[4;32m Invalid Input !!! \e[0m"| lolcat
+echo "Press Enter To Go Home"| lolcat
+read a3
+clear
+fi
+else
+apt install boxes -y
+clear
+echo -e "\e[4;34m This Bomber Was Created By Rafeeqkhan_official\e[0m" | lolcat
+echo -e "\e[1;34m For Any Queries Contact Me!!!\e[0m"| lolcat
+echo -e "\e[1;32m   Intagram:- rafeeqkhan_official\e[0m"| lolcat
+echo " "| lolcat
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"| lolcat
+echo " "| lolcat
+echo "Press 1 To  Start SMS Bomber "| lolcat
+echo "Press 2 To  Start Call Bomber "| lolcat
+echo "Press 3 To  Update "| lolcat
+echo "Press 4 To  View Features "| lolcat
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 bomber.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By Rafeeqkhan_official'> call.xxx
+python3 bomber.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..." | lolcat
+git clone https://github.com/pepperjack1210/Bomber
+if [[ -s Bomber/Bomber.sh ]];then
+cd Bomber
+cp -r -f * .. > temp
+cd ..
+rm -rf  Bomber >> temp
+rm update.rafeeqkhan_official >> temp
+rm temp
+chmod +x Bomber.sh
+fi
+echo -e "\e[1;32m Bomber Will Restart Now..."| lolcat
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."| lolcat
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."| lolcat
+read a6
+./Bomber.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"| lolcat
+figlet Bomb | lolcat
+echo -e "\e[1;34mCreated By \e[1;34m"| lolcat
+toilet -f mono12 -F border rafeeqkhan_official | lolcat
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m" | lolcat
+echo "  [+] Unlimited And Super-Fast Bombing"| lolcat
+echo "  [+] International Bombing"| lolcat
+echo "  [+] Call Bombing "| lolcat
+echo "  [+] Protection List"| lolcat
+echo "  [+] Automated Future Updates"| lolcat
+echo "  [+] Easy To Use And Embed in Code"| lolcat
+echo ""
+echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"| lolcat
+echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"| lolcat
+echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"| lolcat
+echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"| lolcat
+echo  " "
+echo -e "\e[4;31m That's All !!!\e[0m"| lolcat
+echo -e "\e[4;34m This Bomber Was Created By rafeeqkhan_official \e[0m" | lolcat
+echo -e "\e[1;34m For Any Queries Contact Me!!!\e[0m"| lolcat
+echo -e "\e[1;32m   Intagram:- rafeeqkhan_official\e[0m"| lolcat
+echo "Press Enter To Go Home"| lolcat
+read a3
+clear
+else
+echo -e "\e[4;32m Invalid Input !!! \e[0m"| lolcat
+echo "Press Enter To Go Home"| lolcat
+read a3
+clear
+fi
+
+cowsay -f tux "rafeeqkhan_official" | lolcat
+fi
+done
